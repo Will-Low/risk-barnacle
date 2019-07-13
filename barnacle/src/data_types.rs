6cd5b16cd5805dd1274f9eb93d04ffc431c75070
Will-Low <moose.nugget.music@gmail.com>
2019-07-12 21:15:45 -0400

    Adding license info for Triangular code

--- barnacle/src/data_types.rs
+++ barnacle/src/data_types.rs
@@ -436,15 +436,34 @@ impl WeightChecking for Vec<Entry> {
 
 // The Triangular distribution below is:
 //
-// Copyright 2018 Developers of the Rand project.
+// Copyright 2018 Developers of the Rand project
+// Copyright (c) 2014 The Rust Project Developers
 //
-// Licensed under the Apache License, Version 2.0 <LICENSE-APACHE or
-// https://www.apache.org/licenses/LICENSE-2.0> or the MIT license
-// <LICENSE-MIT or https://opensource.org/licenses/MIT>, at your
-// option. This file may not be copied, modified, or distributed
-// except according to those terms.
+// Permission is hereby granted, free of charge, to any
+// person obtaining a copy of this software and associated
+// documentation files (the "Software"), to deal in the
+// Software without restriction, including without
+// limitation the rights to use, copy, modify, merge,
+// publish, distribute, sublicense, and/or sell copies of
+// the Software, and to permit persons to whom the Software
+// is furnished to do so, subject to the following
+// conditions:
 //
-// It has been modified from the original form in rand::distributions::Triangular
+// The above copyright notice and this permission notice
+// shall be included in all copies or substantial portions
+// of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF
+// ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED
+// TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A
+// PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT
+// SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY
+// CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION
+// OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR
+// IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
+// DEALINGS IN THE SOFTWARE.
+//
+// It has been modified from the original in rand::distributions::Triangular
 // to allow for a uniform distribution where min == mode == max.
 
 use rand::Rng;
